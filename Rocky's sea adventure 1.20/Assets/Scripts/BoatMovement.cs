6d8178f0822defe29316c872c1c0c9dc84d4560d
Reynard <reynardcheng@gmail.com>
2018-11-15 15:36:36 +0800

    boat floatin on water effect

--- Rocky's sea adventure 1.20/Assets/Scripts/BoatMovement.cs
+++ Rocky's sea adventure 1.20/Assets/Scripts/BoatMovement.cs
@@ -24,6 +24,11 @@ public class BoatMovement : MonoBehaviour
 	[SerializeField] float boostUsageRate; // this is the rate of decrease of boost
 	[SerializeField] bool isBoosting;
 
+	// to rotate the boat to give it a realistic effect
+	[SerializeField] float floatRotation;
+	[SerializeField] float rotateRate;
+	[SerializeField] bool rotateBack;
+
 	// float on water
 	public float heightAboveWater;
 
@@ -50,6 +55,8 @@ public class BoatMovement : MonoBehaviour
 	{
 
 		// variables
+		
+		// for movement
 		maxSpeed = 8f;
 		moveSpeed = 0f;
 		maxReverseSpeed = -5f;
@@ -61,6 +68,10 @@ public class BoatMovement : MonoBehaviour
 		boost = 100f;
 		boostUsageRate = 20f;
 		isBoosting = false;
+
+		// for floating
+		floatRotation = -90f;
+		rotateRate = 5f;
 
 		// components
 		chController = GetComponent<CharacterController>();
@@ -82,31 +93,13 @@ public class BoatMovement : MonoBehaviour
 
 		BoostSlider();
 
+		BoatRocking();
+
 	}
 
-
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// This part is for movement only
-	void Boost()
-	{
-		if (boost < 0) boost = Mathf.CeilToInt(boost);
-
-		isBoosting = (boost > 0f && Input.GetKey(KeyCode.Space) && theBoat.controllingBoat) ? isBoosting = true : isBoosting = false;
-		maxSpeed = (isBoosting) ? 15f : 8f;
-		moveSpeed = (isBoosting) ? maxSpeed : moveSpeed;
-		if (!isBoosting && moveSpeed > maxSpeed) moveSpeed = 8f;
-
-		boost = isBoosting ? boost -= boostUsageRate * Time.deltaTime : boost;
-
-		if (boost > 100f) boost = 100f; 
-	}
-
-	void StopBoat()
-	{
-		moveMentOutput = transform.TransformDirection(Vector3.down * moveSpeed);
-		if (moveSpeed >= 0) moveSpeed -= deccelerationSpeed * Time.deltaTime;
-		if (moveSpeed <= 0) moveSpeed += deccelerationSpeed * Time.deltaTime;
-	}
-
+	/// 
 	// method that handles the boat movement
 	void Movement()
 	{
@@ -169,6 +162,62 @@ public class BoatMovement : MonoBehaviour
 		transform.rotation = Quaternion.Euler(-90, currentSteerSpeed, 0);
 	}
 
+	void Boost()
+	{
+		if (boost < 0) boost = Mathf.CeilToInt(boost);
+
+		isBoosting = (boost > 0f && Input.GetKey(KeyCode.Space) && theBoat.controllingBoat) ? isBoosting = true : isBoosting = false;
+		maxSpeed = (isBoosting) ? 15f : 8f;
+		moveSpeed = (isBoosting) ? maxSpeed : moveSpeed;
+		if (!isBoosting && moveSpeed > maxSpeed) moveSpeed = 8f;
+
+		boost = isBoosting ? boost -= boostUsageRate * Time.deltaTime : boost;
+
+		if (boost > 100f) boost = 100f;
+	}
+
+	void StopBoat()
+	{
+		moveMentOutput = transform.TransformDirection(Vector3.down * moveSpeed);
+		if (moveSpeed >= 0) moveSpeed -= deccelerationSpeed * Time.deltaTime;
+		if (moveSpeed <= 0) moveSpeed += deccelerationSpeed * Time.deltaTime;
+	}
+
+	//methods to handle the boat animations
+
+	// i did not use animation because it woul affect the steering, cannot steer using animation 
+
+	void BoatRocking()
+	{
+		if (rotateBack)
+		{
+			rotateRate -= 2 * Time.deltaTime;
+			floatRotation -= 3f * Time.deltaTime;
+		}
+		else
+		{
+			rotateRate -= 2 * Time.deltaTime;
+			floatRotation += 3f * Time.deltaTime;
+		}
+
+		if (rotateRate <= 0 && rotateBack)
+		{
+			rotateRate = 5f;
+			rotateBack = false;
+		}
+
+		else if (rotateRate <= 0 && !rotateBack)
+		{
+			rotateRate = 5f;
+			rotateBack = true;
+		}
+
+		transform.localRotation = Quaternion.Euler(floatRotation, currentSteerSpeed, transform.rotation.z);
+
+	}
+	/// 
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 	// this next part is for UI
 	void BoostSlider()
 	{
@@ -185,13 +234,13 @@ public class BoatMovement : MonoBehaviour
 		boostSlider.value = boost;
 	}
 
+
 	private void OnTriggerEnter(Collider other)
 	{
 		if (other.tag == "Boost")
 		{
 			boost += 100;
 			Destroy(other.gameObject);
-
 		}
 	}
 
